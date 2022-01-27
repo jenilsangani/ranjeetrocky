4e5a56e883345b6307934a0dce5b95a38fb5ce85
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-01-27 17:48:40 +0530

    rename readme template file

--- dateUpdater.js
+++ dateUpdater.js
@@ -3,7 +3,7 @@ const { promises: fs } = require("fs");
 async function main() {
 
     const d = new Date();
-    const readmeTemplateFile = (await fs.readFile("./README.md.template")).toString("utf-8");
+    const readmeTemplateFile = (await fs.readFile("./README.temp.md")).toString("utf-8");
     const updatatedReadmeFile = readmeTemplateFile.replace("{{lastUpdatedOn}}", d.toISOString());
     // const date = readmeTemplateFile.replace("{{lastUpdatedOn}}", d.toString().replace("GMT+0530 (India Standard Time)","IST"));
     await fs.writeFile("./README.md", updatatedReadmeFile);
