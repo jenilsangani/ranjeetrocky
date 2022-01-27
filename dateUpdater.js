6700aaea36e48cc74cd74fa5d2ccd69b6698c9fe
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-01-27 17:21:29 +0530

    added date Updater js file

---
+++ dateUpdater.js
@@ -0,0 +1,14 @@
+const { promises: fs } = require("fs");
+
+async function main() {
+
+    const d = new Date();
+    const readmeTemplateFile = (await fs.readFile("./README.md.template")).toString("utf-8");
+    const updatatedReadmeFile = readmeTemplateFile.replace("{{lastUpdatedOn}}", d.toISOString());
+    // const date = readmeTemplateFile.replace("{{lastUpdatedOn}}", d.toString().replace("GMT+0530 (India Standard Time)","IST"));
+    await fs.writeFile("./README.md", updatatedReadmeFile);
+    // const readmeFile = (await fs.readFile('./README.md')).toString("utf-8");
+    // console.log("Readme File Data = "+readmeFile)
+}
+main();
+// setInterval(main, 1000); 
